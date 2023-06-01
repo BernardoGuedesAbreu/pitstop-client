6c2941bf305d0329584fea8fe2377e69770a60d2
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-06-01 16:49:31 +0100

    Nav changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import Navbar from "./Components/Navbar/Navbar";
 import DriverList from "./Components/Drivers";
 
 
 function App() {
   return (
-    <div>
-    <Routes>
-    <Route path="/drivers" element={<DriverList />} />
-    
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path="/drivers" element={<DriverList />} />
+
       </Routes>
-      </div>
+    </div>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './navbar.css';
+
+const sidebarNavItems = [
+    {
+        display: 'Dashboard',
+        to: '/',
+        section: ''
+    },
+    {
+        display: 'Getting Started',
+        to: '/started',
+        section: 'started'
+    },
+    {
+        display: 'Calendar',
+        to: '/calendar',
+        section: 'calendar'
+    },
+    {
+        display: 'User',
+        to: '/user',
+        section: 'user'
+    },
+    {
+        display: 'Orders',
+        to: '/order',
+        section: 'order'
+    },
+]
+
+const Sidebar = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [stepHeight, setStepHeight] = useState(0);
+    const sidebarRef = useRef();
+    const indicatorRef = useRef();
+    const location = useLocation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            const sidebarItem = sidebarRef.current.querySelector('.sidebar__menu__item');
+            indicatorRef.current.style.height = `${sidebarItem.clientHeight}px`;
+            setStepHeight(sidebarItem.clientHeight);
+        }, 50);
+    }, []);
+
+    useEffect(() => {
+        const curPath = window.location.pathname.split('/')[1];
+        const activeItem = sidebarNavItems.findIndex(item => item.section === curPath);
+        setActiveIndex(curPath.length === 0 ? 0 : activeItem);
+    }, [location]);
+
+    return <div className='sidebar'>
+        <div className="sidebar__logo">
+            PitStop
+        </div>
+        <div ref={sidebarRef} className="sidebar__menu">
+            <div
+                ref={indicatorRef}
+                className="sidebar__menu__indicator"
+                style={{
+                    transform: `translateX(-50%) translateY(${activeIndex * stepHeight}px)`
+                }}
+            ></div>
+            {
+                sidebarNavItems.map((item, index) => (
+                    <Link to={item.to} key={index}>
+                        <div className={`sidebar__menu__item ${activeIndex === index ? 'active' : ''}`}>
+                            <div className="sidebar__menu__item__text">
+                                {item.display}
+                            </div>
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
+    </div>;
+};
+
+export default Sidebar
