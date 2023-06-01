bfb41c574c573204b8f7765002bad7fa78b3d62a
BernardoGuedesAbreu <bernardoabreu1991@gmail.com>
2023-06-01 21:18:23 +0100

    endday1

--- src/Components/DriverCard/DriverCard.jsx
+++ src/Components/DriverCard/DriverCard.jsx
@@ -1,19 +1,35 @@
-import DriverList from "../DriverList/Drivers";
+import PropTypes from 'prop-types';
 
-const DriverCard = ({ selectedDriver }) => {
-    return (
-      <div>
-        <h2>Driver Card</h2>
-        <DriverList />
-        {selectedDriver && (
-          <div>
-            <h3>Name: {selectedDriver.name}</h3>
-            <h3>Nationality: {selectedDriver.nationality}</h3>
-            {/* Other driver information */}
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default DriverCard;
+const DriverCard = ({ drivers, selectedDriver }) => {
+  const selectedDriverInfo = drivers.find(
+    (driver) => driver.driverId === selectedDriver
+  );
+
+  return (
+    <div>
+      <h2>Driver Card</h2>
+      {selectedDriverInfo && (
+        <div>
+          <h3>Name: {selectedDriverInfo.givenName} {selectedDriverInfo.familyName}</h3>
+          <h3>Nationality: {selectedDriverInfo.nationality}</h3>
+          {/* Render other driver information */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+DriverCard.propTypes = {
+  drivers: PropTypes.arrayOf(
+    PropTypes.shape({
+      driverId: PropTypes.string,
+      givenName: PropTypes.string,
+      familyName: PropTypes.string,
+      nationality: PropTypes.string,
+      // Add other driver properties
+    })
+  ).isRequired,
+  selectedDriver: PropTypes.string.isRequired
+};
+
+export default DriverCard;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,27 +1,46 @@
-import { useState } from 'react';
-import DriverCard from './DriverCard';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import DriverCard from '../components/DriverCard/DriverCard';
+
+const api = 'http://localhost:5005';
 
 const Dashboard = () => {
   const [selectedDriver, setSelectedDriver] = useState('');
+  const [drivers, setDrivers] = useState([]);
 
   const handleDriverChange = (event) => {
     setSelectedDriver(event.target.value);
   };
+
+  useEffect(() => {
+    async function fetchDrivers() {
+      try {
+        const response = await axios.get(`${api}/api/drivers`);
+        const { drivers } = response.data;
+        console.log('Fetched drivers:', drivers);
+        setDrivers(drivers);
+      } catch (error) {
+        console.error('Error fetching drivers:', error);
+      }
+    }
+
+    fetchDrivers();
+  }, []);
 
   return (
     <div>
       <h2>Dashboard</h2>
       <select value={selectedDriver} onChange={handleDriverChange}>
         <option value="">Select a driver</option>
-        <option value="driver1">Driver 1</option>
-        <option value="driver2">Driver 2</option>
-        <option value="driver3">Driver 3</option>
-        {/* Add more options for each driver */}
+        {drivers.map((driver) => (
+          <option key={driver.driverId} value={driver.driverId}>
+            {driver.givenName} {driver.familyName}
+          </option>
+        ))}
       </select>
-      {selectedDriver && <DriverCard selectedDriver={selectedDriver} />}
+      {selectedDriver && <DriverCard drivers={drivers} selectedDriver={selectedDriver} />}
     </div>
   );
 };
 
 export default Dashboard;
-
